a9960b6660e8c927ca1fdcfc87ac25cd5a969c91
Rohit533-max <rohit03383@gmail.com>
2026-08-04 21:51:10 +0530

    Comment added

--- UI/window.py
+++ UI/window.py
@@ -5,7 +5,7 @@ app = customtkinter.CTk()
 app.title("Personal Discipline Tracker")
 app.geometry("400x300")
 
+# background color
 app.configure(fg_color="#1E1E1E")
 
-#this is main branch 
 app.mainloop()
