2a7172c4a8545002d4e63c22eba5868b0585e78b
Rohit533-max <rohit03383@gmail.com>
2026-08-04 21:39:24 +0530

    UI files

---
+++ UI/window.py
@@ -0,0 +1,11 @@
+import customtkinter
+
+app = customtkinter.CTk()
+
+app.title("Personal Discipline Tracker")
+app.geometry("400x300")
+
+app.configure(fg_color="#1E1E1E")
+
+#this is main branch 
+app.mainloop()
