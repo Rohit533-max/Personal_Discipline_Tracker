318caf7315de79fc6566c7ae1f8f27da38787bd3
Rohit533-max <rohit03383@gmail.com>
2026-08-12 22:24:33 +0530

    day_progress func

--- managers/history.py
+++ managers/history.py
@@ -18,3 +18,38 @@ def get_records_by_date(date):
 
     connection.close()
     return records
+
+def get_history_dates():
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    SELECT DISTINCT date
+    FROM daily_records
+    ORDER BY date DESC""")
+
+    dates = cursor.fetchall()
+
+    connection.close()
+
+    return dates
+
+def get_day_progress(date):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT COUNT(*) FROM daily_records WHERE date = ?",(date,))
+    total = cursor.fetchone()[0]
+
+    cursor.execute("SELECT COUNT(*) FROM daily_records WHERE date = ? AND completed = 1", (date,))
+    completed = cursor.fetchone()[0]
+
+    connection.close()
+
+    if total == 0:
+        percentage = 0
+    else:
+        percentage = (completed / total) * 100
+
+    return completed, total, percentage
+
