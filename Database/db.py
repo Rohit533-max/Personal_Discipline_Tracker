e909de213a9e20cbb2ea809c4b82d58c99c4a456
Rohit533-max <rohit03383@gmail.com>
2026-08-19 22:41:04 +0530

    Daily record table added

--- Database/db.py
+++ Database/db.py
@@ -16,8 +16,29 @@ def create_table():
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         priority TEXT NOT NULL,
-        task_date TEXT NOT NULL,
+        task_date DATE NOT NULL,
         archived INTEGER NOT NULL DEFAULT 0)""")
+
+    connection.commit()
+    connection.close()
+
+def daily_records():
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS daily_record(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task_id INTEGER NOT NULL,
+        date DATE NOT NULL,
+        completed BOOLEAN NOT NULL DEFAULT 0,
+        completed_at DATETIME,
+        
+        FOREIGN KEY (task_id) REFERENCES tasks(id),
+        UNIQUE (task_id,date))""")
+
+    #unique does don't allow same thing to be entered twice
+
 
     connection.commit()
     connection.close()
